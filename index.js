c81a1ad9dc465573ccf59c0a3b934d4f79809030
RyanWood2334 <ryanwooduwcoding.2334@gmail.com>
2023-05-28 12:09:47 -0700

    added routes for getting notes, and initializing the index.html and notes.html

--- index.js
+++ index.js
@@ -12,6 +12,23 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.listen(PORT, () => console.log(`Listening at http://localhost:${PORT}`));
+
 app.get("/notes", (req, res) => {
   res.sendFile(path.join(__dirname, "./public/notes.html"));
+});
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+
+app.get("/api/notes", (req, res) => {
+  fs.readFile("./db/db.json", "utf-8", (err, notesData) => {
+    if (err) {
+      return res.status(500).json({ msg: "error reading db" });
+    } else {
+      const dataArr = JSON.parse(notesData);
+      return res.json(dataArr);
+    }
+  });
 });
